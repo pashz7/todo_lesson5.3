4d4d11be77aa9bb5c688aecd3d4d15aa8f8792ab
pashz7 <pashec1710@gmail.com>
2025-11-24 18:28:44 +0300

    realize loader in all place of app

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -1,5 +1,5 @@
 import type { RequestStatus } from "@/common/types"
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, isFulfilled, isRejected } from "@reduxjs/toolkit"
 
 export const appSlice = createSlice({
   name: "app",
@@ -14,6 +14,25 @@ export const appSlice = createSlice({
     selectAppStatus: (state) => state.status,
     selectAppError: (state) => state.error,
     selectIsLoggedIn: (state) => state.isLoggedIn,
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher((action)=>{
+
+      console.log(action.type)
+      // Пробегается по всем action, даже которые находятся в апи
+       return false
+    },(state, _action)=>{
+     state.status = "loading"
+    })
+      .addMatcher(isFulfilled, (state, _action)=>{
+        state.status = "succeeded"
+        /// есть еще такое дополнительный синтаксис
+      })
+      .addMatcher((action )=>{
+        return action.type.endsWith("/rejected")
+      }, (state, _action)=>{
+        state.status = "failed"
+      })
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
